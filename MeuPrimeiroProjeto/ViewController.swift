569ee26d9746e5d7b2711ef82aebe20404f9edbc
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2021-09-09 02:16:51 -0300

    Modulo 2 Aula 1

--- MeuPrimeiroProjeto/ViewController.swift
+++ MeuPrimeiroProjeto/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-
         // Do any additional setup after loading the view.
     }
 
